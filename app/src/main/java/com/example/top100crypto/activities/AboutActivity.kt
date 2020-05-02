0b6e74422f5c75d5b571ef9ebf659f80bfadbedb
DetaEx <alexdul2013@mail.ru>
2020-05-02 21:55:34 +0300

    created project

---
+++ app/src/main/java/com/example/top100crypto/activities/AboutActivity.kt
@@ -0,0 +1,15 @@
+package com.example.top100crypto.activities
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.top100crypto.R
+
+class AboutActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_about)
+
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
+    }
+}
